9dbe393a0419c52b442d83c22cd70fca213da73c
Doogle <fdunigan11@gmail.com>
2018-07-30 17:00:37 -0400

    read files for products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-var mySql = require("mysql");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
@@ -6,7 +6,20 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "root",
-    database: "top_songsDB"
+    database: "bamazon_DB"
   });
 
+  connection.connect(function(err) {
+    if (err) throw err;
+    displayInfo();
+  });
   
+  function displayInfo() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+      console.log("ID: " + res[i].id + " | Product Name: " + res[i].product_name + " | Price: " + res[i].price);
+      }
+      connection.end();
+    });
+  }
