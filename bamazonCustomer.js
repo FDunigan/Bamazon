4e4098949ee35eb7a12a0797a3d5ccfd15c65ec5
Doogle <fdunigan11@gmail.com>
2018-07-30 22:53:51 -0400

    display changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,13 +13,44 @@ var connection = mysql.createConnection({
     if (err) throw err;
     displayInfo();
   });
+
+  function idSearch() {
+      inquirer
+        .prompt([
+        {
+            name: "item",
+            type: "input",
+            message: "Please enter the ID for the product you wish to purchase:",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+        },
+        {
+            name: "unitsearch",
+            type: "input",
+            message: "How many units of the product would you like?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+            }
+         
+        ])
+    };
   
   function displayInfo() {
     connection.query("SELECT * FROM products", function(err, res) {
       if (err) throw err;
       for (var i = 0; i < res.length; i++) {
-      console.log("ID: " + res[i].id + " | Product Name: " + res[i].product_name + " | Price: " + res[i].price);
+      console.log("ID: " + res[i].id + " | Product Name: " + res[i].product_name + " | Price: " + res[i].price + 
+        " | Quantity Remaining: " + res[i].stock_quantity);
       }
-      connection.end();
+      idSearch();
     });
   }
+
